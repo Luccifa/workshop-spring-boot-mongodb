869574429bb215c1a711846461d2df65bac67133
Fabio Akira Lucci <fabiolucci@terra.com.br>
2021-04-01 19:27:46 -0300

    Criando entity Post com user alinhado

---
+++ src/main/java/com/luccifa/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.luccifa.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luccifa.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
