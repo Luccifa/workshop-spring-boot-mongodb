3fd0cabb17b80759b51fa91e5594438f0f3e0842
Fabio Akira Lucci <fabiolucci@terra.com.br>
2021-04-01 22:30:02 -0300

    Consulta com varios criterios

--- src/main/java/com/luccifa/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/luccifa/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.luccifa.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -17,4 +18,14 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 
+	// ?0 - primeiro parametro
+	// ?1 - segundo parametro
+	// ?2 - terceiro parametro
+	// i = ignore CaseSensitive
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , "
+			+ "{ $or: [ { 'title': { $regex: ?0, $options: 'i' } },"
+		 	        + " { 'body': { $regex: ?0, $options: 'i' } }, "
+			         + "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+	
 }
